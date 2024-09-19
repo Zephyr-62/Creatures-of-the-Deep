291f769c3b5bdec5de2ecc809a0d3800c7e7473a
Andrej Erdelsky <erd.andrej@gmail.com>
2024-09-19 22:18:35 +0200

    Add scriptable object artefacts
    Artefacts now work as scriptable object, the name of the 3d model is used as an id which can then used to retrieve the info about the artifact from a cache of all artefacts

--- Assets/_MyAssets/Scripts/ArtefactPickupSensor.cs
+++ Assets/_MyAssets/Scripts/ArtefactPickupSensor.cs
@@ -1,25 +1,44 @@
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class ArtefactPickupSensor : MonoBehaviour
 {
-    [SerializeField] string tagFilter = "Artefact";
-    [SerializeField] UnityEvent onTriggerEnter;
-    [SerializeField] UnityEvent onTriggerExit;
+    [SerializeField] private UnityEvent onTriggerEnter;
+    [SerializeField] private UnityEvent onTriggerExit;
+    
+    private readonly string _tagFilter = "Artefact";
+    private List<Collider> _colliders = new();
+    
     private void OnTriggerEnter(Collider other)
     {
-        if (other.GameObject().CompareTag(tagFilter))
+        if (other.GameObject().CompareTag(_tagFilter))
         {
+            _colliders.Add(other);
             onTriggerEnter.Invoke();
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.GameObject().CompareTag(tagFilter))
+        if (other.GameObject().CompareTag(_tagFilter))
         {
+            _colliders.Remove(other);
             onTriggerExit.Invoke();
         }
     }
+
+    public void PickUpArtefact()
+    {
+        GameObject artefact = _colliders[0].gameObject;
+        ScriptableArtefact artefactInfo = ScriptableArtefact.GetArtefactInfo(artefact.name);
+        
+        _colliders.Clear();
+        Destroy(artefact);
+        
+        print("Picked up " + artefactInfo.artName + " artefact!");
+    }
+    
+    
 }
