3daab1a4c1122737d10dc0190eaca868032bc26f
Andrej Erdelsky <erd.andrej@gmail.com>
2024-09-23 18:34:22 +0200

    Add uniform sampling for the mine field
    Added Poisson Disc Sampling for uniformly distributing the mines

--- Assets/_MyAssets/Scripts/MineField.cs
+++ Assets/_MyAssets/Scripts/MineField.cs
@@ -1,26 +1,44 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MineField : MonoBehaviour
 {
     [SerializeField] private GameObject hazardObject;
-    [SerializeField] private int spawnCount = 20;
+    [SerializeField] [Range(2f, 10f)] public float radius = 5;
     [SerializeField] private float yStartPosition = 5f;
     [SerializeField] private float xWidth = 100f;
     [SerializeField] private float yHeight = 10f;
     [SerializeField] private float zDepth = 100f;
+    [SerializeField] private int rejectionSamples = 30;
+
+    private List<Vector2> _points;
+
+    void OnValidate()
+    {
+        _points = PoissonDiscSampling.GeneratePoints(radius, new Vector2(xWidth, zDepth), rejectionSamples);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        foreach (Vector2 point in _points)
+        {
+            Gizmos.DrawWireSphere(new Vector3(point.x - xWidth / 2, yStartPosition, point.y - zDepth / 2), 1);
+        }
+    }
 
     private void Start()
     {
         transform.position = new Vector3(transform.position.x, yStartPosition, transform.position.z);
-        for (var i = 0; i < spawnCount; i++) SpawnHazardObject();
+        foreach (Vector2 point in _points) SpawnHazardObject(point);
     }
 
-    private void SpawnHazardObject()
+    private void SpawnHazardObject(Vector2 point)
     {
-        var spawnPointX = Random.Range(transform.position.x - xWidth / 2, transform.position.x + xWidth / 2);
-        var spawnPointY = Random.Range(transform.position.y, transform.position.y + yHeight);
-        var spawnPointZ = Random.Range(transform.position.z - zDepth / 2, transform.position.z + zDepth / 2);
-
-        Instantiate(hazardObject, new Vector3(spawnPointX, spawnPointY, spawnPointZ), Quaternion.identity, transform);
+        var position = new Vector3(
+            point.x - xWidth / 2,
+            Random.Range(transform.position.y, transform.position.y + yHeight),
+            point.y - zDepth / 2);
+        Instantiate(hazardObject, position, Quaternion.identity, transform);
     }
 }
