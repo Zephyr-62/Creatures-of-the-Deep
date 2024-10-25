2a8c4e38655740906c7fb3fb780f8dbfa87de8e2
Andrej Erdelsky <erd.andrej@gmail.com>
2024-10-25 12:18:47 +0200

    Add environment culling

---
+++ Assets/_MyAssets/Scripts/EnvironmentAreaCulling.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class EnvironmentAreaCulling : MonoBehaviour
+{
+    [SerializeField] private BoxCollider boxCollider;
+    private Renderer[] _renderers;
+    private Collider[] _colliders;
+    
+    private void Awake()
+    {
+        _renderers = GetComponentsInChildren<Renderer>();
+        _colliders = GetComponentsInChildren<Collider>();
+    }
+
+    private void Start()
+    {
+        SetChildrenRendering(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Enter");
+        if (other.gameObject.TryGetComponent(out SubmarinePhysicsSystem _))
+        {
+            SetChildrenRendering(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exit");
+        if (other.gameObject.TryGetComponent(out SubmarinePhysicsSystem _))
+        {
+            SetChildrenRendering(false);
+        }
+    }
+
+    private void SetChildrenRendering(bool enable)
+    {
+        foreach (var childRenderer in _renderers)
+        {
+            childRenderer.enabled = enable;
+        }
+            
+        foreach (var childCollider in _colliders)
+        {
+            childCollider.enabled = enable;
+        }
+        
+        boxCollider.enabled = true;
+    }
+}
